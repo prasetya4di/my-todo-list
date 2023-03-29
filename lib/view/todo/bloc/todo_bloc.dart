9c8199e45970d3db2cd94d919a6298d55ad9b3cf
Pras <prasetyaarfanto@gmail.com>
2023-03-29 19:23:38 +0700

    Use unmodifiable list view for todos

--- lib/view/todo/bloc/todo_bloc.dart
+++ lib/view/todo/bloc/todo_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_todo_list/data/model/entity/todo.dart';
 import 'package:my_todo_list/domain/add_todo.dart';
@@ -12,7 +14,7 @@ class TodoBloc extends Bloc<TodoEvent, TodoState> {
 
   List<Todo> _todos = [];
 
-  List<Todo> get todos => _todos;
+  UnmodifiableListView<Todo> get todos => UnmodifiableListView(_todos);
 
   TodoBloc(this._addTodo, this._finishTodo, this._getTodo)
       : super(const TodoState.initial()) {
