b21c416a482adcb032aaac9b58de8dfd20d38477
Pras <prasetyaarfanto@gmail.com>
2023-03-29 08:00:49 +0700

    Add objectbox

---
+++ lib/data/source/local/objectbox.dart
@@ -0,0 +1,17 @@
+import 'package:my_todo_list/objectbox.g.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+
+class ObjectBox {
+  late final Store store;
+
+  ObjectBox._create(this.store);
+
+  static Future<ObjectBox> create() async {
+    final docsDir = await getApplicationDocumentsDirectory();
+    final store =
+        await openStore(directory: p.join(docsDir.path, "obx-my-todo-list"));
+
+    return ObjectBox._create(store);
+  }
+}
