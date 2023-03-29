b808ad87866d06f007353d8643d62901ab47aa31
Pras <prasetyaarfanto@gmail.com>
2023-03-29 08:30:59 +0700

    Add todo screen

---
+++ lib/view/todo/todo_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TodoScreen extends StatelessWidget {
+  const TodoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text("Todo screen"),
+    );
+  }
+}
