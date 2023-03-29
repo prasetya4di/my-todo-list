ae26d78c272deedce468fcfb2381529c25005961
Pras <prasetyaarfanto@gmail.com>
2023-03-29 10:59:05 +0700

    Add empty todo widget

--- lib/view/todo/todo_screen.dart
+++ lib/view/todo/todo_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_todo_list/view/todo/widgets/empty_todo.dart';
 
 class TodoScreen extends StatelessWidget {
   const TodoScreen({super.key});
@@ -6,8 +7,17 @@ class TodoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Text("Todo screen"),
+      appBar: AppBar(
+        title: const Text("Todo list"),
+      ),
+      body: const EmptyTodo(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed function here
+        },
+        backgroundColor: Theme.of(context).primaryColor,
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
---
+++ lib/view/todo/widgets/empty_todo.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:my_todo_list/generated/assets.dart';
+
+class EmptyTodo extends StatelessWidget {
+  const EmptyTodo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24),
+      child: Column(
+        children: [
+          SvgPicture.asset(Assets.imagesEmptyTodo, width: 250),
+          const SizedBox(height: 20),
+          Text(
+              "Looks like you're all caught up! No todos to show. Why not take a break and enjoy some free time? Or, if you're feeling productive, click the plus button to start adding some new todos and manage your tasks like a pro!",
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.titleMedium),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 12.0),
+                child:
+                    SvgPicture.asset(Assets.imagesIcArrowDownBlue, width: 100),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
